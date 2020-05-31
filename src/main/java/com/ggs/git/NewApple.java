06342edfa755394ac20e0d1819c397aa06cf27e4
eclipse_user <eclipse_user@ggs.com>
2020-05-31 19:26:00 +0800

    modify java file

--- src/main/java/com/ggs/git/NewApple.java
+++ src/main/java/com/ggs/git/NewApple.java
@@ -3,5 +3,6 @@ package com.ggs.git;
 public class NewApple {
 	public static void main(String[] args) {
 		System.out.println("xxxxxxx");
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 	}
 }
